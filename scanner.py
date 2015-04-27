f426df884086ebafa4bba74282957dafe3f86e8d
James Warren <jwarren116@gmail.com>
2015-04-26 18:02:45 -0700

    Initial commit and outline of command line parser (resource: Violent Python)

---
+++ scanner.py
@@ -0,0 +1,24 @@
+import optparse
+import socket
+
+parser = optparse.OptionParser('usage %prog -H <target host> -p <target port>')
+parser.add_option('-H',
+                  dest='tgtHost',
+                  type='string',
+                  help='specify target host'
+                  )
+
+parser.add_option('-p',
+                  dest='tgtPort',
+                  type='int',
+                  help='specify target port'
+                  )
+
+(options, args) = parser.parse_args()
+
+tgtHost = options.tgtHost
+tgtPort = options.tgtPort
+
+if tgtHost is None or tgtPort is None:
+    print parser.usage
+    exit(0)
