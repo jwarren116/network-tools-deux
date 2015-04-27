de833e259d71748815d3e9771f3a6efb584a7386
James Warren <jwarren116@gmail.com>
2015-04-26 18:10:35 -0700

    Add portScan and connScan functions

--- scanner.py
+++ scanner.py
@@ -22,3 +22,30 @@ tgtPort = options.tgtPort
 if tgtHost is None or tgtPort is None:
     print parser.usage
     exit(0)
+
+
+def connScan(tgtHost, tgtPort):
+    try:
+        connSkt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        connSkt.connect((tgtHost, tgtPort))
+        print '[+]%d/tcp open' % tgtPort
+        connSkt.close()
+    except: # fix bare exception!!!!!!!!!!!!
+        print '[-]%d/tcp closed' % tgtPort
+
+
+def portScan(tgtHost, tgtPorts):
+    try:
+        tgtIP = socket.gethostbyname(tgtHost)
+    except: # here too
+        print "[-] Cannot resolve '%s': Unknown host" % tgtHost
+        return
+    try:
+        tgtName = socket.gethostbyaddr(tgtIP)
+        print '\n[+] Scan results for: %s' % tgtName[0]
+    except: # make it stop!
+        print '\n[+] Scan results for: %s' % tgtIP
+    socket.setdefaulttimeout(1)
+    for tgtPort in tgtPorts:
+        print 'Scanning port %d' % tgtPort
+        connScan(tgtHost, int(tgtPort))
