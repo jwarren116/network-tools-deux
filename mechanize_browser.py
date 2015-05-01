f126dc408533234b2c7ea0fd7b16b776ee3352d3
James Warren <jwarren116@gmail.com>
2015-05-01 13:01:12 -0700

    Add cookie printer

--- mechanize_browser.py
+++ mechanize_browser.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env
 import mechanize
+import cookielib
 
 
 def test_agent(url, user_agent):
@@ -9,7 +10,18 @@ def test_agent(url, user_agent):
     source_code = page.read()
     print source_code
 
-user_agent = [('User-agent','Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01')]
-url = 'http://whatsmyuseragent.com/'
+
+def print_cookies(url):
+    browser = mechanize.Browser()
+    cookie_jar = cookielib.LWPCookieJar()
+    browser.set_cookiejar(cookie_jar)
+    page = browser.open(url)
+    for cookie in cookie_jar:
+        print cookie
+
+
+user_agent = [('User-agent', 'Mozilla/5.0 (X11; U; Linux 2.4.2-2 i586; en-US; m18) Gecko/20010131 Netscape6/6.01')]
+url = 'http://www.jwarren.co/http/'
 
 test_agent(url, user_agent)
+print_cookies(url)
